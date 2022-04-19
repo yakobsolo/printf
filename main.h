ebfb71f38156ab494b957a60233de86eec5b3d9c
MubarekHussen <mubahussen2014@gmail.com>
2022-04-19 22:44:16 +0300

    Advanced

--- main.h
+++ main.h
@@ -11,15 +11,16 @@ int print_rev(va_list r);
 int print_bin(va_list b);
 int print_unsig(va_list u);
 int print_octal(va_list o);
+int print_hex(unsigned int n, unsigned int c);
 int print_x(va_list x);
 int print_X(va_list X);
 int print_rot13(va_list R);
 /**
-  * struct code_format - Struct format
-  *
-  * @var: The specifiers
-  * @func: The function associated
-  */
+ * struct code_format - Struct format
+ *
+ * @var: The specifiers
+ * @func: The function associated
+ */
 typedef struct code_format
 {
 	char *var;
